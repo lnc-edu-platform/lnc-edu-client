138cd3f85a1453ff4b2a0c1145e6e7bdac120fea
최연우 <florau@knu.ac.kr>
2026-05-27 14:11:35 +0900

    Implement notice page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { DefaultLayout } from '../components/layout/DefaultLayout.jsx';
-import { HomePage } from '../pages/home/HomePage.jsx';
-import { LoginPage } from '../pages/login/LoginPage.jsx';
+import { DefaultLayout } from './components/layout/DefaultLayout.jsx';
+import { ProtectedRoute } from './components/route/ProtectedRoute.jsx';
+import { HomePage } from './pages/home/HomePage.jsx';
+import { LoginPage } from './pages/login/LoginPage.jsx';
+import { NoticeDetailPage } from './pages/notice/NoticeDetailPage.jsx';
+import { NoticePage } from './pages/notice/NoticePage.jsx';
+import { PlaceholderPage } from './pages/placeholder/PlaceholderPage.jsx';
 
 function App() {
   return (
@@ -9,6 +13,23 @@ function App() {
       <Routes>
         <Route element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="notice" element={<NoticePage />} />
+          <Route path="notice/:noticeId" element={<NoticeDetailPage />} />
+          <Route
+            path="volunteer"
+            element={<PlaceholderPage title="봉사처" />}
+          />
+          <Route
+            path="community"
+            element={<PlaceholderPage title="커뮤니티" />}
+          />
+          <Route path="members" element={<PlaceholderPage title="구성원" />} />
+          <Route element={<ProtectedRoute />}>
+            <Route
+              path="mypage"
+              element={<PlaceholderPage title="마이페이지" />}
+            />
+          </Route>
         </Route>
         <Route path="/login" element={<LoginPage />} />
       </Routes>
